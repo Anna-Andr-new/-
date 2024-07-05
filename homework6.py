741945d4541c056bfb8be2b1867c74479e97c0a5
Anna-Andr-new <tsukanova0910anna@gmail.com>
2024-07-05 17:55:16 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict={'Anna':1984,'Leva':2006, 'Iliana':2010, 'Varvara':2015}
+print(my_dict)
+print(my_dict.get('Anna'))
+print(my_dict.get('Alina'))
+my_dict.update({'Soso': 2001, 'Faina': 1996})
+print(my_dict)
+del my_dict['Anna']
+print(my_dict)
+my_set={'apple', 'dog', 'frog', 'cat','dog', 'frog','cat'}
+print(my_set)
+print(my_set.add(165))
+print(my_set.add('smile'))
+print(my_set)
+print(my_set.remove(165))
+print(my_set)
